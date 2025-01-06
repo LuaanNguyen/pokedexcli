8a0fcc826f4591f9c38bc3ebff4b2cc7c24e3db1
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-05 21:37:41 -0700

    Separated functions

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main () {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		//Use fmt.Print to print the prompt Pokedex > without a newline character
-		fmt.Print("Pokedex > ")
-
-		if !scanner.Scan() {
-			break
-		}
-
-		//Use the scanner's .Scan and .Text methods to get the user's input as a string
-		input := scanner.Text()
-
-		//Clean the users input by trimming any leading or trailing whitespace, and converting it to lowercase.
-		stringSlice := cleanInput(input)
-
-		// If there are no words, just continue to the next iteration
-		if len(stringSlice) == 0 {
-			continue
-		}
-		
-		//Capture the first "word" of the input and use it to print
-		firstWord := stringSlice[0]
-		fmt.Printf("Your command was: %s\n", firstWord)
-	}
+	startRepl()
 }
 
 
--- repl.go
+++ repl.go
@@ -1,6 +1,37 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		//Use fmt.Print to print the prompt Pokedex > without a newline character
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		//Use the scanner's .Scan and .Text methods to get the user's input as a string
+		input := scanner.Text()
+
+		//Clean the users input by trimming any leading or trailing whitespace, and converting it to lowercase.
+		stringSlice := cleanInput(input)
+
+		// If there are no words, just continue to the next iteration
+		if len(stringSlice) == 0 {
+			continue
+		}
+		
+		//Capture the first "word" of the input and use it to print
+		firstWord := stringSlice[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 //This function splits the users' input into "words" based on white space.
 //It should also lowercase the input and trim any leading or trailing whitespace
