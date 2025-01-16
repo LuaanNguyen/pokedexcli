74bfa4121b155edf606d3e146a36f3ca0e2b8bb7
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-15 17:27:41 -0700

    Add mapb function

--- command_map.go
+++ command_map.go
@@ -8,10 +8,12 @@ import (
 )
 
 type LocationResponse struct {
-	Results []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
+    Results  []struct {
+        Name string `json:"name"`
+        URL  string `json:"url"`
+    } `json:"results"`
+    Next     *string `json:"next"`    // Pointer type because it can be null
+    Previous *string `json:"previous"` // Pointer type because it can be null
 }
 
 
@@ -45,6 +47,16 @@ func commandMap(cfg *config) error {
 			fmt.Printf("%s\n", location.Name)
 		}
 	}
+
+	// Update the config for pagination
+    cfg.Next = ""
+    if locations.Next != nil {
+        cfg.Next = *locations.Next // Set the Next URL if it exists
+    }
+    cfg.Previous = ""
+    if locations.Previous != nil {
+        cfg.Previous = *locations.Previous // Set the Previous URL if it exists
+    }
 
 	return nil
 }
--- command_mapb.go
+++ command_mapb.go
@@ -1,7 +1,52 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func commandMapb(cfg *config) error {
+	url := cfg.Previous 
+	
+	if url == "" {
+		fmt.Println("You are on the first page.")
+		return nil 
+	}
 
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("error fetching previous Podedex locations: %w", err)
+	}
 
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response's body: %w", err)
+	}
+	defer res.Body.Close() 
+
+	var locations LocationResponse
+	err = json.Unmarshal(body, &locations)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling response: %w", err)
+	}
+
+	if len(locations.Results) > 0 {
+		for _, location := range locations.Results {
+			fmt.Printf("%s\n", location.Name)
+		}	
+	}
+
+	// Update the config for pagination
+    cfg.Next = ""
+    if locations.Next != nil {
+        cfg.Next = *locations.Next // Set the Next URL if it exists
+    }
+    cfg.Previous = ""
+    if locations.Previous != nil {
+        cfg.Previous = *locations.Previous // Set the Previous URL if it exists
+    }
+	
 	return nil
 }
